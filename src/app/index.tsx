5cd3ec24e51dc41897200d8469f5584fed07f0f1
hyeonseo1109 <alsgustj2@naver.com>
2026-05-10 05:05:16 +0900

    design: gloabal css

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,5 @@
 import { LoginPage } from "@/pages/login-page/ui";
+import "./global.css.ts";
 
 export function App() {
   return (
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 import { createRoot } from "react-dom/client";
-import "@/app/global.css";
 import { App } from "@/app";
 
 createRoot(document.getElementById("root")!).render(<App />);
